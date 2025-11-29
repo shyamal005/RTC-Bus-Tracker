5217aeb9b7d1a13ebdd4b5a6970a976fd6743f09
Shyamal Kudupudi <96627688+shyamal005@users.noreply.github.com>
2025-11-30 01:25:15 +0530

    Update mockdata.js

--- backend/mockdata.js
+++ backend/mockdata.js
@@ -3,5 +3,6 @@ module.exports = {
     { vehicle: 'AP11Z1234', type: 'METRO EXPRESS', route: '1Z', eta: '11:30:00' },
     { vehicle: 'AP11Z5678', type: 'ORDINARY', route: '222A', eta: '11:35:00' }
   ],
-  // Add more stops
+ 
+
 };
